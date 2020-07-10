48a4fdd19e1ce0db681f50cc47cb2b9f98492941
wjy5446 <wjy5446@gmail.com>
2020-07-10 20:50:34 +0900

    feat) add convert_value_format function

--- search_engine/milvusdb.py
+++ search_engine/milvusdb.py
@@ -3,9 +3,10 @@ import numpy as np
 
 from milvus import Milvus, IndexType, MetricType, Status
 
+
 class SearchEngine:
     def __init__(self, host, port):
-        self.host = os.environ.get('MILVUS_HOST',host)
+        self.host = os.environ.get('MILVUS_HOST', host)
         self.port = os.environ.get('MILVUS_PORT', str(port))
         self.engine = Milvus(host=self.host, port=self.port)
         self.collection_name = None
@@ -18,8 +19,8 @@ class SearchEngine:
         # collection 생성
 
         param = {'collection_name': collection_name,
-                'dimension': dimension,
-                 'index_file_size': 1000, 
+                 'dimension': dimension,
+                 'index_file_size': 1000,
                  'metric_type': MetricType.IP}
 
         self.engine.create_collection(param)
@@ -30,9 +31,9 @@ class SearchEngine:
         # collection 삭제
 
         self.engine.drop_collection(collection_name=collection_name)
-        
+
         print('[INFO] collection {}을 삭제했습니다.'.format(collection_name))
-    
+
     def get_collection_stats(self, collection_name):
         # collection 정보 출력
 
@@ -40,7 +41,7 @@ class SearchEngine:
         print(self.engine.get_collection_stats(collection_name))
 
     def set_collection(self, collection_name):
-        # 쿼리 조작을 하기 위한 collection 지정  
+        # 쿼리 조작을 하기 위한 collection 지정
 
         self.collection_name = collection_name
         print('[INFO] setting collection {}'.format(self.collection_name))
@@ -58,12 +59,19 @@ class SearchEngine:
 
         self.check_set_collection()
 
-        _, vector = self.engine.get_entity_by_id(collection_name=self.collection_name, ids=key)
-        
+        _, vector = self.engine.get_entity_by_id(
+            collection_name=self.collection_name, ids=key)
+
         return True if vector[0] else False
 
-    def convert_key_format(key):
+    def convert_key_format(self, key):
         return [key] if isinstance(key, int) else key
+
+    def convert_value_format(self, value):
+        rank = len(value.shape)
+
+        assert rank < 2, '[ERROR] value의 dim을 2 미만으로 입력해 주세요!!'
+        return value.reshape(1, -1) if rank == 1 else value
 
 #################################################
 # INSERT
@@ -73,14 +81,16 @@ class SearchEngine:
         # 데이터를 collection에 입력
 
         key = self.convert_key_format(key)
-        
+        value = self.convert_value_format(value)
+
         if self.check_exist_data_by_key(key):
             print("[ERROR] 이미 collection에 데이터가 존재합니다.")
-            return 
+            return
 
-        self.engine.insert(collection_name=self.collection_name, records=value, ids=key)
+        self.engine.insert(
+            collection_name=self.collection_name, records=value, ids=key)
         self.engine.flush([self.collection_name])
-        
+
         print('[INFO] insert key {}'.format(key))
 
 #################################################
@@ -91,16 +101,16 @@ class SearchEngine:
         # 데이터를 collection에서 제거
 
         key = self.convert_key_format(key)
-        
+
         if not self.check_exist_data_by_key(key):
             print("[ERROR] collection에 데이터가 존재하지 않습니다.")
-            return 
-        
+            return
+
         self.engine.delete_entity_by_id(self.collection_name, key)
         self.engine.flush([self.collection_name])
-        
+
         print('[INFO] delete key {}'.format(key))
-    
+
 
 #################################################
 # UPDATE
@@ -108,19 +118,21 @@ class SearchEngine:
 
     def update_data(self, key, value):
         # 데이터를 업데이트
-        
+
         key = self.convert_key_format(key)
+        value = self.convert_value_format(value)
 
         if not self.check_exist_data_by_key(key):
             print("[ERROR] collection에 데이터가 존재하지 않습니다.")
-            return 
+            return
 
         self.engine.delete_entity_by_id(self.collection_name, key)
         self.engine.flush([self.collection_name])
 
-        self.engine.insert(collection_name=self.collection_name, records=value, ids=key)
+        self.engine.insert(
+            collection_name=self.collection_name, records=value, ids=key)
         self.engine.flush([self.collection_name])
-        
+
         print('[INFO] update key {}'.format(key))
 
 #################################################
@@ -129,15 +141,18 @@ class SearchEngine:
 
     def search_by_feature(self, feature, top_k):
         # feature를 이용해서 데이터를 검색
-        
+
         self.check_set_collection()
-        
-        feature = self.convert_key_format(feature)
 
-        _, result = self.engine.search(collection_name=self.collection_name, query_records=feature, top_k=top_k)
+        feature = self.convert_value_format(feature)
 
-        li_id = [list(map(lambda x: x.id, result[0])) for i in range(len(result))]
-        li_dist = [list(map(lambda x: x.distance, result[0])) for i in range(len(result))]
+        _, result = self.engine.search(
+            collection_name=self.collection_name, query_records=feature, top_k=top_k)
+
+        li_id = [list(map(lambda x: x.id, result[0]))
+                 for i in range(len(result))]
+        li_dist = [list(map(lambda x: x.distance, result[0]))
+                   for i in range(len(result))]
 
         return li_id, li_dist
 
@@ -150,11 +165,15 @@ class SearchEngine:
         if not self.check_exist_data_by_key(key):
             print("[ERROR] collection에 데이터가 존재하지 않습니다.")
             return
-        
-        _, vector = self.engine.get_entity_by_id(collection_name=self.collection_name, ids=key)
-        _, result = self.engine.search(collection_name=self.collection_name, query_records=vector, top_k=top_k+1)
 
-        li_id = [list(map(lambda x: x.id, result[0][1:])) for i in range(len(result))]
-        li_dist = [list(map(lambda x: x.distance, result[0][1:])) for i in range(len(result))]
+        _, vector = self.engine.get_entity_by_id(
+            collection_name=self.collection_name, ids=key)
+        _, result = self.engine.search(
+            collection_name=self.collection_name, query_records=vector, top_k=top_k+1)
+
+        li_id = [list(map(lambda x: x.id, result[0][1:]))
+                 for i in range(len(result))]
+        li_dist = [list(map(lambda x: x.distance, result[0][1:]))
+                   for i in range(len(result))]
 
         return li_id, li_dist
