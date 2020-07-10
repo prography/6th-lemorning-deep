ba5177b5d201a4304fb22b9ef8ba88b136e44293
wjy5446 <wjy5446@gmail.com>
2020-07-10 20:50:58 +0900

    fix) bugfix

--- test_search_engine.py
+++ test_search_engine.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
                    for file in os.listdir(path_root)]
     path_new_audio = 'data/audio/joram-moments_of_clarity-08-solipsism-59-88.mp3'
 
-    HOST = os.environ.get('HOST_SEARCH_ENGINE', 'localhost')
+    HOST = os.environ.get('HOST_SEARCH_ENGINE', '18.219.188.129')
     PORT = os.environ.get('PORT_SEARCH_ENGINE', 19530)
     N_DIM_FEATURE = os.environ.get('N_DIM_FEATURE', 753)
 
@@ -21,10 +21,10 @@ if __name__ == '__main__':
     # Collection
     ##############################
     # 2-1. create collection
-    engine.create_engine('musicDB', N_DIM_FEATURE)
+    engine.create_collection('musicDB', N_DIM_FEATURE)
 
     # 2-2. show info of collection
-    engine.get_collection_stats()
+    engine.get_collection_stats('musicDB')
 
     # 2-3. delete collection
     engine.drop_collection('musicDB')
@@ -39,27 +39,28 @@ if __name__ == '__main__':
     for i, path_audio in enumerate(path_audios):
         feature = model.extract_info(path_audio, mode='feature')
         engine.insert_data(i, feature)
-    engine.get_collection_stats()
+    engine.get_collection_stats('musicDB')
 
     # 3-3. update data
     feature = model.extract_info(path_new_audio, mode='feature')
-    engine.update_data(len(path_audio), feature)
-    engine.get_collection_stats()
+    engine.update_data(len(path_audios) - 1, feature)
+    engine.get_collection_stats('musicDB')
 
-    # 3-3. update data
-    engine.delete_data(len(path_audios))
-    engine.get_collection_stats()
+    # # 3-3. update data
+    engine.delete_data(len(path_audios) - 1)
+    engine.get_collection_stats('musicDB')
 
-    ##############################
-    # Search Data
-    ##############################
+    # ##############################
+    # # Search Data
+    # ##############################
 
-    # 4-1. search data by feature
+    # # 4-1. search data by feature
+    feature = model.extract_info(path_new_audio, mode='feature')
     li_id, li_distance = engine.search_by_feature(feature, 5)
     result = [(idx, dis) for idx, dis in zip(li_id, li_distance)]
     print(result)
 
     # 4-2. search data by key
-    li_id, li_distance = engine.search_by_key(len(path_audio) - 1, 5)
+    li_id, li_distance = engine.search_by_key(len(path_audios) - 2, 5)
     result = [(idx, dis) for idx, dis in zip(li_id, li_distance)]
     print(result)
