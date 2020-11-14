2fc9ae5b0f7dfbd72299c52734a418d1286d22e1
Yuna Son <reisei88.dev@gmail.com>
2020-11-14 21:28:41 +0900

    audiofile upload 추가

--- rending/application.py
+++ rending/application.py
@@ -1,63 +1,56 @@
 from flask import Flask, redirect, render_template, request, url_for, flash
-
 import os
 from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-musics = []
-UPLOAD_FOLDER = './music'
-ALLOWED_EXTENSIONS = {'m4a', 'mp3', 'wav', 'm4p'}
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+UPLOAD_FOLDER = './uploads/audio'
+ALLOWED_EXTENSIONS = set(['m4a', 'm4p', 'wav', 'mp3', 'mp4'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
 def index():
-    return "<h1>API rending page HTML ver</h1>"
+    return '''
+    <h1>API rending page</h1>
+        <ul>/file</ul>
+        <ul>/tag</ul>
+        <ul>/recommend</ul>
+    '''
 
 
-@app.route("/submit", methods=["GET", "POST"])
-def submit():
-    if request.method == "POST":
-        music = request.form.get("FileName")
-        musics.append(music)
-        return redirect("/tag")
-    else:
-        return render_template("submit.html")
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+
+@app.route('/file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "<h1>Ok!! </h1>"
+        else:
+            return '''
+            <!doctype html>
+            <title>Upload new File</title>
+            <h1>File Select Error!</h1>
+            <a href="/file">file</a>
+            '''
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 
 @app.route("/tag")
